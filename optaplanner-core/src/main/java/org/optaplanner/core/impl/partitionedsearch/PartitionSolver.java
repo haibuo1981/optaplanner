8977e505d1ae164ffbde1e912706e8bb4359d615
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2018-04-18 14:43:33 +0200

    PLANNER-1090 Partitioned Search: accurate score calculation speed

--- optaplanner-core/src/main/java/org/optaplanner/core/impl/partitionedsearch/PartitionSolver.java
+++ optaplanner-core/src/main/java/org/optaplanner/core/impl/partitionedsearch/PartitionSolver.java
@@ -135,4 +135,8 @@ public class PartitionSolver<Solution_> extends AbstractSolver<Solution_> {
         // TODO log?
     }
 
+    public long getScoreCalculationCount() {
+        return solverScope.getScoreCalculationCount();
+    }
+
 }
